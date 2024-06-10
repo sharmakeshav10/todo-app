136c78a31f5d9ef2baed6cfb5db8820362de3f63
Keshav Sharma <sharmakeshav0708@gmail.com>
2024-06-11 00:41:50 +0530

    setup project structure

---
+++ backend/types.js
@@ -0,0 +1,10 @@
+const zod = require("zod");
+
+const createTodo = zod.object({
+  title: zod.string(),
+  description: zod.string(),
+});
+
+const updateTodo = zod.object({
+  id: zod.string(),
+});
